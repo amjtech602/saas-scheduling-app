0846a016ff19bce357ea1da359975c73650247e3
Antonio Filho <atnfilho@yahoo.com.br>
2025-08-23 15:43:41 -0300

    Correção de bug em book/professionlId/page

--- app/book/[professionalId]/page.tsx
+++ app/book/[professionalId]/page.tsx
@@ -1,14 +1,12 @@
-"use client"
 
-import { BookingFlow } from "@/components/booking/booking-flow"
+import { BookingFlow } from "@/components/booking/booking-flow";
 
 interface BookingPageProps {
-  params: {
-    professionalId: string
-  }
+  params: Promise<{ professionalId: string }>
 }
 
 
-export default function BookingPage({ params }: BookingPageProps) {
-  return <BookingFlow professionalId={params.professionalId} />
+export default async function BookingPage({ params }: BookingPageProps) {
+  const { professionalId } = await params;
+  return <BookingFlow professionalId={professionalId} />
 }
